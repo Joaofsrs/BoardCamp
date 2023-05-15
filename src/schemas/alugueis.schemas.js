0194df35673e742575fa3cc30dabf278700d6aa2
Joaofsrs <rsjoao12@gmail.com>
2023-05-15 17:25:31 -0300

    correcao para entrega delete rentals

--- src/schemas/alugueis.schemas.js
+++ src/schemas/alugueis.schemas.js
@@ -3,5 +3,5 @@ import joi from "joi";
 export const rentalSchema = joi.object({
     customerId: joi.number().integer().required(),
     gameId: joi.number().integer().required(),
-    daysRented: joi.number().integer().required()
+    daysRented: joi.number().integer().min(1).required()
 })
