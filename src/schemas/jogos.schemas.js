c0d7fe7968f91e94e72eeec684b82adce226a694
Joaofsrs <rsjoao12@gmail.com>
2023-05-15 16:39:38 -0300

    correcao para entrega

--- src/schemas/jogos.schemas.js
+++ src/schemas/jogos.schemas.js
@@ -3,6 +3,6 @@ import joi from "joi";
 export const gameSchema = joi.object({
     name: joi.string().required(),
     image: joi.string().required(),
-    stockTotal: joi.number().integer().required(),
+    stockTotal: joi.number().integer().min(1).required(),
     pricePerDay: joi.number().required()
 })
